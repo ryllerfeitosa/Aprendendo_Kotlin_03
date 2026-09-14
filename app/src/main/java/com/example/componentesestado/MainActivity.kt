e82f29363a10b007c49a3f5de5e102d8d47117fd
RILLER DA COSTA FEITOSA <25203652@dsn127m1246734.cfp127aluno.local>
2026-09-14 11:15:23 -0300

    Projeto componente estado finalizado

--- app/src/main/java/com/example/componentesestado/MainActivity.kt
+++ app/src/main/java/com/example/componentesestado/MainActivity.kt
@@ -1,25 +1,32 @@
 package com.example.componentesestado
 
 import android.R.attr.text
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.content.MediaType.Companion.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.componentesestado.ui.theme.ComponentesEstadoTheme
 import com.example.componentesestado.ui.theme.Righteus
@@ -54,9 +61,9 @@ fun basicComponentsScreen(modifier: Modifier = Modifier) {
                 fontFamily = FontFamily.Serif,
                 color = Color(0xFF3DDC84),
                 modifier = Modifier
-                    .background(Color.Magenta)
                     .fillMaxWidth(),
-                textAlign = TextAlign.End
+                textAlign = TextAlign.End,
+                letterSpacing = 4.sp
             )
             Text(
                 text = "com Jetpack Compose",
@@ -66,5 +73,22 @@ fun basicComponentsScreen(modifier: Modifier = Modifier) {
                 fontFamily = Righteus,
                 modifier = Modifier.align(Alignment.CenterHorizontally)
             )
+        AndroidEnemy(
+            color = Color.Red,
+            modifier = Modifier.size(100.dp)
+        )
     }
+}
+
+@Composable
+fun AndroidEnemy(
+    color: Color,
+    modifier: Modifier = Modifier
+){
+    Image(
+        modifier = modifier,
+        painter = painterResource(R.drawable.ic_launcher_foreground),
+        colorFilter = ColorFilter.tint(color),
+        contentDescription = "Android Enemy"
+    )
 }
