1764f3c3c35c140f110b12fb0627a04dea750c68
RILLER DA COSTA FEITOSA <25203652@dsn127m1246734.cfp127aluno.local>
2026-09-14 10:56:54 -0300

    Aprendendo a estrutura do composable no kotlin

---
+++ app/src/main/java/com/example/componentesestado/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.componentesestado
+
+import android.R.attr.text
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.componentesestado.ui.theme.ComponentesEstadoTheme
+import com.example.componentesestado.ui.theme.Righteus
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComponentesEstadoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    basicComponentsScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun basicComponentsScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier   //Permite editar e mudar os padroes da coluna
+            .fillMaxSize()
+            .background(Color(239, 247, 207))
+    ) {
+            Text(
+                text = "Aulas android",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Serif,
+                color = Color(0xFF3DDC84),
+                modifier = Modifier
+                    .background(Color.Magenta)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.End
+            )
+            Text(
+                text = "com Jetpack Compose",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color(66,134,244),
+                fontFamily = Righteus,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+    }
+}
